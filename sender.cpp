e7644ebb2677d4c01e78c99e0af1eceb3da587f3
Brad <bradpf@outlook.com>
2016-07-27 22:09:30 -0400

    Need to figure out how to communicate with outside IP

--- sender.cpp
+++ sender.cpp
@@ -4,14 +4,25 @@
 using namespace std;
 
 #define QUIT "quit"
+#define QUEUE_NAME "lanQueue"
+#define QUEUE_KEY "exampleQueueKey"
+#define EXCHANGE_NAME "exampleExchangeName"
 
 int main()  {
 
-  string message;
+  // Perform setup
+  AmqpClient::Channel::ptr_t channel = AmqpClient::Channel::Create();
+
+  channel->DeclareQueue(QUEUE_NAME);
+  channel->BindQueue(QUEUE_NAME, EXCHANGE_NAME, QUEUE_KEY);  // This needs to be changed
+
+  AmqpClient::BasicMessage::ptr_t message;
 
   do {
     cout << "What message would you like to send to your friend?\n";
     cin >> message;
+
+
 
   } while (message.compare(QUIT));
 
