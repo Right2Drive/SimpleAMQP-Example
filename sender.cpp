f779da8d0cfe96be926ac6700fd611b9b83185fd
Brad <bradpf@outlook.com>
2016-08-04 21:27:21 -0400

    Sending messages to rabbitMQ

--- sender.cpp
+++ sender.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 #define QUIT "quit"
-#define QUEUE_NAME "lanQueue"
-#define QUEUE_KEY "exampleQueueKey"
-#define EXCHANGE_NAME "exampleExchangeName"
+#define QUEUE_NAME "brad-queue"
+#define ROUTING_KEY "exampleQueueKey"
+#define EXCHANGE_NAME "brad-exchange"
 
 int main()  {
 
   // Perform setup
-  AmqpClient::Channel::ptr_t channel = AmqpClient::Channel::Create();
+  AmqpClient::Channel::ptr_t channel = AmqpClient::Channel::Create("localhost");
 
   channel->DeclareQueue(QUEUE_NAME);
-  channel->BindQueue(QUEUE_NAME, EXCHANGE_NAME, QUEUE_KEY);  // This needs to be changed
+  channel->BindQueue(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);  // This needs to be changed
 
   AmqpClient::BasicMessage::ptr_t message;
 
@@ -22,7 +22,7 @@ int main()  {
     cout << "What message would you like to send to your friend?\n";
     cin >> message;
 
-
+    channel->BasicPublish(EXCHANGE_NAME, ROUTING_KEY, message);
 
   } while (message.compare(QUIT));
 
