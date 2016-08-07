7d00210d7c9234eb40898dd80bb2bed1fe4e9c87
Brad <bradpf@outlook.com>
2016-08-07 01:10:28 -0400

    Trying to resolve library failures when compiling using makefile

--- receiver.cpp
+++ receiver.cpp
@@ -1,30 +1,30 @@
-#include <SimpleAmqpClient/SimpleAmqpClient.h>
-#include <string>
+  #include <SimpleAmqpClient/SimpleAmqpClient.h>
+  #include <string>
 
-using namespace std;
+  using namespace std;
 
-#define QUEUE_NAME "brad-queue"
-#define ROUTING_KEY "exampleQueueKey"
-#define EXCHANGE_NAME "brad-exchange"
-#define CONSUMER_TAG "brad-consumer"
+  #define QUEUE_NAME "brad-queue"
+  #define ROUTING_KEY "exampleQueueKey"
+  #define EXCHANGE_NAME "brad-exchange"
+  #define CONSUMER_TAG "brad-consumer"
 
-int main()  {
+  int main()  {
 
-  cout << "Receiving messages now\n";
+    cout << "Receiving messages now\n";
 
-   // Perform setup
-  //AmqpClient::Channel::ptr_t channel = AmqpClient::Channel::Create("localhost");
+    // Perform setup
+    AmqpClient::Channel::ptr_t channel = AmqpClient::Channel::Create("localhost");
 
-  //channel->DeclareQueue(QUEUE_NAME);
-  //channel->BindQueue(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);  // This needs to be changed
+    channel->DeclareQueue(QUEUE_NAME);
+    channel->BindQueue(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);  // This needs to be changed
 
-  //channel->BasicConsume(QUEUE_NAME, CONSUMER_TAG);
+    channel->BasicConsume(QUEUE_NAME, CONSUMER_TAG);
 
-  //AmqpClient::BasicMessage::ptr_t message;
+    AmqpClient::BasicMessage::ptr_t message;
 
-  // while (1) {
-  //   cout << endl << "message: " << channel->BasicConsumeMessage(CONSUMER_TAG)->Message()->Body() << endl;
-  // }
+    while (1) {
+      cout << endl << "message: " << channel->BasicConsumeMessage(CONSUMER_TAG)->Message()->Body() << endl;
+    }
 
-  return 0;
-}
+    return 0;
+  }
